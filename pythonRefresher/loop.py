0bc13f5687539b0dd89abe1fbe26010c70c717a2
aryan kaul <aryankaul2001@gmail.com>
2024-05-04 14:37:37 +0530

    loops

---
+++ pythonRefresher/loop.py
@@ -0,0 +1,15 @@
+my_list = [1, 2, 3, 4, 5, 6]
+
+
+for x in my_list:
+    print(x, end=' ')
+print()
+for x in range(1, 10):
+    print(x, end=' ')
+print()
+
+i = 0
+
+while i < 5:
+    print(i, end=' ')
+    i += 1
