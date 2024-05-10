14978b65a222458648778ad20e1511aaef7b4911
aryan kaul <aryankaul2001@gmail.com>
2024-05-10 12:11:35 +0530

    query params

--- Section 05/books.py
+++ Section 05/books.py
@@ -43,9 +43,20 @@ async def read_all_books():
     return Books
 
 
+# Path parameters
 @app.get('/books/{index}')
 async def get_book_by_index(index: int):
     print('index: ', index)
     if 0 <= index < len(Books):
         return Books[index]
     return "Invalid Index"
+
+
+# Query Params
+@app.get("/books/")
+async def get_book_by_category(category: str):
+    books_to_return = []
+    for book in Books:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
